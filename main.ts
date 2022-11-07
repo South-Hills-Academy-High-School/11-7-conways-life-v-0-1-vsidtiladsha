8fda9fdc35f01429b7ecab5d6dd3f91375a7cd2c
vsidtiladsha <113388330+vsidtiladsha@users.noreply.github.com>
2022-11-07 14:26:42 -0800

    working on it

--- main.ts
+++ main.ts
@@ -1,1 +1,62 @@
-
+function drawgrid () {
+    gridsprites = []
+    current_y = 0
+    for (let row of grid) {
+        for (let griddpace of row) {
+            if (griddpace == 1) {
+                gridsprite = sprites.create(img`
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 2 2 2 2 7 7 2 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 2 2 2 2 2 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 2 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 2 7 7 7 7 7 7 7 2 7 7 7 
+                    7 7 7 7 2 2 2 2 2 2 2 2 2 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+                    `, SpriteKind.Player)
+                gridsprite.setPosition(current_x, current_y)
+                gridsprites.push(gridsprite)
+            }
+            current_x += 10
+        }
+        current_y += 10
+    }
+}
+let current_x = 0
+let gridsprite: Sprite = null
+let current_y = 0
+let gridsprites: Sprite[] = []
+let grid: number[][] = []
+let row_template = [
+0,
+1,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0
+]
+let row: number[] = []
+for (let index = 0; index < 16; index++) {
+    grid.push(row_template)
+}
+game.onUpdate(function () {
+    drawgrid()
+})
